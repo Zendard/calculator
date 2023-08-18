88a2784e21963408490a8ba08fb8a0cf9d191abd
Zendard <thijsmuylaert@hotmail.com>
2023-08-18 12:09:44 +0200

    Add correction button

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const subtract = document.getElementsByName("subtract");
 const multiply = document.getElementsByName("multiply");
 const divide = document.getElementsByName("divide");
 const equals = document.getElementsByName("equals");
+const correct = document.getElementsByName("correct");
 
 let operation = [];
 let input = "";
@@ -75,4 +76,14 @@ equals[0].addEventListener("click", () => {
 	});
 	console.log(operation);
 	calculate();
+});
+
+correct[0].addEventListener("click", () => {
+	if (parseInt(input.slice(-2, -1)).toString() == "NaN") {
+		input = input.slice(0, -2);
+	} else {
+		input = input.slice(0, -1);
+	}
+
+	updateDisplay();
 });
